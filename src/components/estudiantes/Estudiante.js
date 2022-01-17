f3559914e27b9f21433a2060856ed9d7adfe61f0
bablybaqrtasri <chouaibelasri@gmai.com>
2022-01-17 10:49:17 +0100

    modificar la Configuración pass

--- src/components/estudiantes/Estudiante.js
+++ src/components/estudiantes/Estudiante.js
@@ -11,12 +11,10 @@ export default class Estudiante extends Component
             <tr>
                 <td>{this.props.estudiante.identificacion}</td>
                 <td>{this.props.estudiante.nombre}</td>
-                <td>******</td>
                 <td>
                     { (this.props.estudiante.editable === true || typeof this.props.estudiante.editable === 'undefined') ? <button className="btn btn-outline-secondary" onClick={() => this.props.onEdit(this.props.estudiante)} title="Editar" > <BsFillPencilFill /> </button> : ""}
                     { (this.props.estudiante.editable === true || typeof this.props.estudiante.editable === 'undefined') ? <button className="btn btn-outline-secondary" onClick={() => this.props.onDelete(this.props.estudiante)} title="Eliminar" ><BsFillTrashFill /> </button> : ""}
-                    { (this.props.estudiante.editable === true || typeof this.props.estudiante.editable === 'undefined') ? <SubirArchivoAS3 estudiante={this.props.estudiante} onImageAdded={this.props.onImageAdded} /> : ""}
-                    { (this.props.estudiante.editable === true || typeof this.props.estudiante.editable === 'undefined') ? <ImagenIdentificacion estudiante={this.props.estudiante} /> : ""}
+                    
                 </td>
             </tr>
         );
